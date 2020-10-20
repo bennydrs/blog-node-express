808e0688649207b6d077a70763e63702cfcd8a48
bennydrspc <bennydrm16@gmail.com>
2020-10-20 11:13:52 +0700

    update blog to mongodb

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -25,6 +25,29 @@ const store = (req, res) => {
     })
 }
 
+const edit = (req, res) => {
+  const id = req.params.id
+  Blog.findById(id)
+    .then(result => {
+      res.render('blog/edit', { title: 'Edit', blog: result})
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('404', { title: 'Blog not found' })
+    })
+}
+
+const update = (req, res) => {
+  const id = req.params.id
+  Blog.findByIdAndUpdate(id, req.body, {new:true})
+    .then(() => {
+      res.redirect('/blogs')
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
 const show = (req, res) => {
   const id = req.params.id
   Blog.findById(id)
@@ -52,6 +75,8 @@ module.exports = {
   index,
   create,
   store,
+  edit,
+  update,
   show,
   destroy
 }
--- routes/blog.js
+++ routes/blog.js
@@ -5,8 +5,9 @@ const blogController = require('../controllers/blogController')
 router.get('/', blogController.index)
 router.get('/create', blogController.create)
 router.post('/create', blogController.store)
+router.get('/edit/:id', blogController.edit)
 router.get('/:id', blogController.show)
+router.post('/:id', blogController.update)
 router.delete('/:id', blogController.destroy)
-
 
 module.exports = router
