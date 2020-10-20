dabab09458ed2abe965a9c650c0442d147ab1229
bennydrspc <bennydrm16@gmail.com>
2020-10-20 10:20:59 +0700

    create, read and delete blog from mongodb

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,23 +1,51 @@
-
+const Blog = require('../models/blog')
 
 const index = (req, res) => {
-  res.render('blog/index', {title: 'Home'})
+  Blog.find().sort({ createdAt: -1 })
+    .then(result => {
+      res.render('blog/index', { title: 'Home', blogs: result })
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 const create = (req, res) => {
-  res.render('blog/create', {title: 'Create'})
+  res.render('blog/create', { title: 'Create a new blog' });
 }
 
 const store = (req, res) => {
-
+  const blog = new Blog(req.body)
+  blog.save()
+    .then(() => {
+      res.redirect('/blogs')
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 const show = (req, res) => {
-  res.render('blog/show', {title: 'Show'})
+  const id = req.params.id
+  Blog.findById(id)
+    .then(result => {
+      res.render('blog/show', { title: 'Show', blog: result })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('404', { title: 'Blog not found' })
+    })
 }
 
 const destroy = (req, res) => {
-
+  const id = req.params.id
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' })
+    })
+    .catch(err => {
+      console.log(err)
+    })
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express')
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 const blogRoutes = require('./routes/blog')
 
 // express app
 const app = express()
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://beda:testblog123@blog.dncxi.mongodb.net/node-blog'
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(3000))
+  .catch(err => console.log(err))
+
 // register view engine ejs
 app.set('view engine', 'ejs')
 
-// listen for request
-app.listen(3000)
+// middleware & static files
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use((req, res, next) => {
+  res.locals.path = req.path;
+  next();
+});
 
 // blog routes
 app.use('/blogs', blogRoutes)
